056469739d32814b83b7e60d914396117534e02b
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2024-01-21 16:03:48 +0100

    task 8 done

--- js/task-07.js
+++ js/task-07.js
@@ -3,4 +3,5 @@ const text = document.querySelector("#text");
 
 input.addEventListener("input", () => {
   text.style.fontSize = input.value + "px";
+  // console.log(input.value);
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.target;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Wszystkie pola powinny zostać uzupełnione!");
+  } else {
+    console.log(`E-mail: ${email.value}, Password: ${password.value}`);
+  }
+  event.target.reset();
+});
